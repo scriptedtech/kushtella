41ffb2b604704e51cdc8c38e623ea2f50c95a425
ikhakoo <ikhakoo@gmail.com>
2016-04-26 15:15:37 -0400

    add some extra methods to increase and decrease quantities

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,64 @@ class OrdersController < ApplicationController
 	  redirect_to root_path, :notice => "Basket emptied successfully."
 	end
 
+	def remove_item
+   item = current_order.order_items.find(params[:id])
+    if current_order.order_items.count == 1
+      destroy
+    else
+      item.remove
+      redirect_to basket_path, :notice => "Item has been removed from your basket successfully"
+    end
+  end
+
+  def increase_item_quantity
+  	item = current_order.order_items.find(params[:id])
+  	amount = params[:amount].to_i
+  	if amount > item.quantity && amount <= item.ordered_item.stock
+  		if item.quantity == 1
+  			amount = amount - 1
+	  			amount.times do
+						item.increase!
+				end
+				redirect_to basket_path, :notice => "Quantity has been updated successfully."
+  		else
+  			amount = amount - item.quantity
+	  		amount.times do
+					item.increase!
+				end
+				redirect_to basket_path, :notice => "Quantity has been updated successfully."
+			end
+		elsif amount > item.ordered_item.stock
+			redirect_to basket_path, :alert => "Unfortunately, we don't have enough stock. We only have #{item.ordered_item.stock} items available at the moment. Please get in touch though, we're always receiving new stock."
+		else
+			item.increase!
+			redirect_to basket_path, :notice => "Quantity has been updated successfully."
+		end
+  rescue Shoppe::Errors::NotEnoughStock => e
+    redirect_to basket_path, :alert => "Unfortunately, we don't have enough stock. We only have #{item.ordered_item.stock} items available at the moment. Please get in touch though, we're always receiving new stock."
+  end
+
+  def decrease_item_quantity
+    item = current_order.order_items.find(params[:id])
+		item.decrease!
+    redirect_to basket_path, :notice => "Quantity has been updated successfully."
+  rescue Shoppe::Errors::NotEnoughStock => e
+    redirect_to basket_path, :alert => "Unfortunately, we don't have enough stock. We only have #{item.ordered_item.stock} items available at the moment. Please get in touch though, we're always receiving new stock."
+  end
+
+  def bulk_update_quanity
+  	bulk = params[:bulk]
+  	item = current_order.order_items.find(params[:id])
+  	if bulk.to_i > item.available_stock.to_i
+	  	bulk.to_i.times do
+	  		item.increase!
+	  	end
+  		redirect_to basket_path, :notice => "Quantity has been updated successfully."
+  	end
+  	rescue Shoppe::Errors::NotEnoughStock => e
+    	redirect_to basket_path, :alert => "Unfortunately, we don't have enough stock. We only have #{item.ordered_item.stock} items available at the moment. Please get in touch though, we're always receiving new stock."
+  end
+
 	def checkout
   @order = Shoppe::Order.find(current_order.id)
 	  if request.patch?
